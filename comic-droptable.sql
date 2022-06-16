eb63b5fe11d865d8cd98751c90a4dbd747647793
krylex-365 <63896058+krylex-365@users.noreply.github.com>
2022-06-16 15:17:14 +0700

    up

---
+++ comic-droptable.sql
@@ -0,0 +1,18 @@
+--use master
+--drop database if exists WEBCOMIC
+--create database WEBCOMIC
+
+use WEBCOMIC
+drop table ComicNotification
+drop table Comment
+drop table FollowComic
+drop table CateComic
+drop table AuthorComic
+drop table ChapterImage
+drop table Chapter
+drop table Author
+drop table Account
+drop table UserDetail
+drop table TranslateTeam
+drop table Category
+drop table Comic
